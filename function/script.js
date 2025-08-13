427859367d54248d17770ec08c8f9fa500c3cf7a
SanjayNakum <sanjaynakum4545@gmail.com>
2025-08-13 16:41:40 +0530

    function & IIFE

---
+++ function/script.js
@@ -0,0 +1,24 @@
+// greeting();
+
+
+// function greeting(){
+//     console.log("hello guys");
+
+// }
+
+
+// let dock = () => console.log("hello");
+
+// dock()
+
+
+(function () {
+    console.log("i will immediately invoke");
+})();
+
+// let greeting = "hello";
+
+// with parameter
+(function (msg) {
+    console.log(msg);
+})("hello");ṇṇṇṇṁ
