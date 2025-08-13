5dea061aec856927b8c4ee5167c52b8ec3ab0ca4
SanjayNakum <sanjaynakum4545@gmail.com>
2025-08-13 10:44:41 +0530

    operators

---
+++ operators/6.typeof_op.js
@@ -0,0 +1,33 @@
+// typeof operator define the data types of variable
+
+// let a = "5";
+
+// console.log(typeof a);
+
+// type conversion
+
+// number + string ->  number will be converted to string so final op will be -> string
+
+// number - string -> string will be converted to number so final op will be -> number
+
+// implicit
+
+// explicit
+
+let a = 5;
+
+let b = "7";
+
+let result = a + b;
+
+console.log(typeof result);
+
+let result3 = a - b;
+
+console.log(result3);
+
+// // explicit
+
+// let result2 = a + Number(b);
+
+// console.log(result2);
